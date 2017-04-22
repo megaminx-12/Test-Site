2dd06d821641ff4af82c05797f07cae1b2a94bae
megaminx-12 <i.megaminx@gmail.com>
2017-04-22 16:31:12 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 function main() {
+	$('.fresh').hide();
+	$('.edgy').hide();
+	$('.dank').hide();
+	$('.old').hide();
 	
+	$('#fresh-btn').on('click', function() {
+		$('.dank').hide();
+		$('.edgy').hide();
+		$('.old').hide();
+		$('.fresh').slideToggle(1000);
+	});
+	$('#edgy-btn').on('click', function() {
+		$('.dank').hide();
+		$('.fresh').hide();
+		$('.old').hide();
+		$('.edgy').slideToggle(1000);
+	});
+	$('#dank-btn').on('click', function() {
+		$('.edgy').hide();
+		$('.fresh').hide();
+		$('.old').hide();
+		$('.dank').slideToggle(1000);
+	});
+	$('#old-btn').on('click', function() {
+		$('.dank').hide();
+		$('.fresh').hide();
+		$('.edgy').hide();
+		$('.old').slideToggle(1000);
+	});
 }
 $(document).ready(main);
